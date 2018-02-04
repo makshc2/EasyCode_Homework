e1f3655553c96c79ee8694db3047b87ae2ab3740
makshc2 <makshc2@gmail.com>
2018-02-04 22:50:55 +0200

    Home work 4

---
+++ Домашняя работа №4/DOM/myscript.js
@@ -0,0 +1,18 @@
+// Доступ к DOM:
+// задание 1
+console.log(document.head);
+
+// задание 2
+
+console.log(document.body);
+
+// задание 3
+
+console.log(document.body.childNodes);
+
+// задание 4
+console.log(document.body.children[0]);
+// a:
+console.log(document.body.children[0].children);
+//b:
+console.log(document.body.children[0].children);
---
+++ Домашняя работа №4/HomeWork4Array.js
@@ -0,0 +1,171 @@
+//Задачи на методы массивов
+
+//Задача №1
+
+function doubleArray(array) {
+    let newArr = [].concat(array);
+    for ( let i =0; i < array.length; i++ ){
+        newArr.push(array[i]);
+    }
+    return newArr;
+}
+
+doubleArray(['one', 'two', 'three', 4, 5]);
+
+//Задача №2
+
+function lastElement(array) {
+    return array[array.length -1];
+}
+
+lastElement(['one','two', 3, 4, 'five']);
+
+//Задача №3
+
+function getArray(num) {
+    if( typeof num !== 'number' && typeof num === 'undefined' ) return new Error ('No number');
+    let newArr = [];
+    for (let i = 1; i <= num; i++) {
+        newArr.push(i);
+    }
+    return newArr;
+}
+
+getArray(10);
+
+//Задача №4
+
+// function changeCollection() {
+//     let newArr = [];
+//     for (let i = 0; i < arguments.length; i++) {
+//         for (let j = 0; j < arguments[i].length; j++) {
+//             newArr.push(arguments[i].shift());
+//         }
+//     }
+//     return newArr;
+// }
+//
+// changeCollection([1, 2, 3], ['a', 'b', 'c']);
+
+//Массивы
+
+// Задача №1
+
+function stringSort(str) {
+    return str.split('').sort().reverse().join('');
+}
+
+stringSort('bcdaeflmjgkhi');
+
+
+//Задача №2
+
+function sortArrayReverse(arr) {
+      return arr.sort ((previous, next) => previous - next).reverse();
+}
+sortArrayReverse([2, 4, 7, 1, -2, 10, -9]);
+
+//Задача №3
+
+function getNewArray(arr, a, b) {
+    let newArr = arr.slice(a, b + 1); // метод slice вырезает часть массива с - по, но не включает послений элемент,
+                                      // для этого было использавано b+1
+    return newArr;
+    }
+
+getNewArray(['a', 'b', 'c', 'd', 'e', 'f'], 2, 4);
+
+// Задача №4
+
+function doubleArray(array) {
+    return (`${array} ${array.concat('')}`);
+}
+
+doubleArray(['one', 2, 'three', 4]);
+
+// Задача №5
+
+function deleteEl(arr) {
+    arr.splice(2,2);
+    return arr;
+}
+
+deleteEl([1, 2, 3, 4, 5]);
+
+// Задача №6
+
+function addNewEl(arr) {
+    arr.splice(2,2, 'there', 'four');
+    return arr;
+}
+
+addNewEl([1, 2, 3, 4, 5]);
+
+// Задача №7
+
+function addNewElement(arr) {
+    arr.splice(3,0, 'awesome');
+    return arr;
+}
+
+addNewElement(['I', 'am', 'an', 'array']);
+
+// Задача №8
+
+function sortArrayInArr(arr) {
+    return arr.sort ((previous, next) => previous.length - next.length);
+}
+
+sortArrayInArr([
+    [14,45],
+    [1],
+    ['a','b','c']
+]);
+
+// Задача №9
+
+function copyArr(arr) {
+    let newArr = arr.concat();
+    return newArr;
+}
+
+copyArr(['I', 'am', 'an', 'array']);
+
+// Задача №10
+
+function sortCore(arr) {
+    return arr.sort ((previous, next) => previous.info.cores - next.info.cores);
+}
+
+sortCore([
+    {cpu:'intel', info:{cores: 2, cache: 3}},
+    {cpu:'intel', info:{cores: 4, cache: 4}},
+    {cpu:'amd', info:{cores: 1, cache: 1}},
+    {cpu:'intel', info:{cores: 3, cache: 2}},
+    {cpu:'intel', info:{cores: 4, cache: 2}}
+]);
+
+
+// Задача №11
+
+const products = [
+    {title: 'prod1', price: 5.2},
+    {title: 'prod2', price: 0.18},
+    {title: 'prod3', price: 15},
+    {title: 'prod4', price: 25},
+    {title: 'prod5', price: 18.9},
+    {title: 'prod6', price: 8},
+    {title: 'prod7', price: 19},
+];
+
+function filterCollection(arr, priceMin, priceMax) {
+    let newCollection = [];
+    for (let i = 0; i < arr.length; i++) {
+        let value = arr[i].price;
+        if( value >= priceMin && value <= priceMax ){
+            newCollection.push(arr[i]);
+        }
+    }
+    return newCollection.sort ((previous, next) => previous.price - next.price);
+}
+filterCollection(products, 15, 30);
