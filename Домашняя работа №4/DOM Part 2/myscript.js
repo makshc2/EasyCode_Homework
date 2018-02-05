cc12613e3eb7c3824455ecc2de3285fcb7811d32
makshc2 <makshc2@gmail.com>
2018-02-05 21:26:04 +0200

    fix bugs

--- Домашняя работа №4/DOM Part 2/myscript.js
+++ Домашняя работа №4/DOM Part 2/myscript.js
@@ -7,16 +7,17 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     isParent(document.body.children[0], document.querySelector('mark'));
     isParent(document.querySelector('ul'), document.querySelector('mark'));
+
+    // Задача 2.
+    console.log(document.querySelector('a'));
+
+    // Задача 3.
+    console.log(document.querySelector('ul').previousElementSibling);
+    console.log(document.querySelector('ul').nextElementSibling);
+
+    // Задача 4.
+    let mylist = document.querySelector('ul');
+
+    console.log(mylist.childElementCount);
 });
 
-// Задача 2.
-console.log(document.querySelector('a'));
-
-// Задача 3.
-console.log(document.querySelector('ul').previousElementSibling);
-console.log(document.querySelector('ul').nextElementSibling);
-
-// Задача 4.
-let mylist = document.querySelector('ul');
-
-console.log(mylist.childElementCount);
