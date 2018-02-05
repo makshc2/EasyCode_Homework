3d08c98485f88503993e3066049bb34425544ef5
makshc2 <makshc2@gmail.com>
2018-02-05 21:21:56 +0200

    Home work 4 DOM part 2

---
+++ Домашняя работа №4/DOM Part 2/myscript.js
@@ -0,0 +1,22 @@
+// DOM задачи
+// Задача 1.
+document.addEventListener('DOMContentLoaded', function () {
+    console.clear();
+    function isParent(parent, child) {
+        return parent.contains(child);
+    }
+    isParent(document.body.children[0], document.querySelector('mark'));
+    isParent(document.querySelector('ul'), document.querySelector('mark'));
+});
+
+// Задача 2.
+console.log(document.querySelector('a'));
+
+// Задача 3.
+console.log(document.querySelector('ul').previousElementSibling);
+console.log(document.querySelector('ul').nextElementSibling);
+
+// Задача 4.
+let mylist = document.querySelector('ul');
+
+console.log(mylist.childElementCount);
