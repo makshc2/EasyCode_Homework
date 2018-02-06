8968646c992fa8c1eefa131371089e03a26db1fb
makshc2 <makshc2@gmail.com>
2018-02-06 21:25:25 +0200

    fix bugs

--- Домашняя работа №4/DOM Alert/myscript.js
+++ Домашняя работа №4/DOM Alert/myscript.js
@@ -1,15 +1,15 @@
 // DOM дополнительная задача
 
 function showDiv(text) {
-   let div = document.querySelector('.alert-info');
-   let buttons = document.querySelector('.button')
-   div.textContent = text;
-   div.classList.add('show');
-   buttons.classList.add('hidden')
-   setTimeout(( hideDiv ) => {
-       div.classList.remove('show')
-       buttons.classList.remove('hidden')
-   }, 2500);
-   
+    let div = document.querySelector('.alert-info');
+    let buttons = document.querySelector('.button')
+    div.textContent = text;
+    div.classList.add('show');
+    buttons.classList.add('hidden');
+    setTimeout(() => {
+        div.classList.remove('show');
+        buttons.classList.remove('hidden')
+    }, 2500);
+}
    //вызов функции привязан к кнопке с классом btn-danger (небольшая модификация от себя:))
 
