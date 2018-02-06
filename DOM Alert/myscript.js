a4fe8b252b099e0804e05c80818cea51f2787e6d
Maksim Shevjakov <makshc2@gmail.com>
2018-02-06 11:49:00 +0200

    Update myscript.js

--- DOM Alert/myscript.js
+++ DOM Alert/myscript.js
@@ -2,10 +2,14 @@
 
 function showDiv(text) {
    let div = document.querySelector('.alert-info');
+   let buttons = document.querySelector('.button')
    div.textContent = text;
    div.classList.add('show');
+   buttons.classList.add('hidden')
    setTimeout((hideDiv) => {
        div.classList.remove('show')
+       buttons.classList.remove('hidden')
    }, 2500);
-}
+   
+   //вызов функции привязан к кнопке с классом btn-danger (небольшая модификация от себя:))
 
