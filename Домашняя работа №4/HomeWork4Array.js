509294cdd8fd0d6bf1c76d63fedd88f4d69755a1
makshc2 <makshc2@gmail.com>
2018-02-07 22:24:07 +0200

    arrow function

--- Домашняя работа №4/HomeWork4Array.js
+++ Домашняя работа №4/HomeWork4Array.js
@@ -21,7 +21,7 @@ lastElement(['one','two', 3, 4, 'five']);
 //Задача №3
 
 function getArray(num) {
-    if( typeof num === 'undefined' || typeof num === 'NaN' || typeof num === 'Infinity' || Math.sign(num) === -1 ) return new Error ('No number');
+    if( typeof num === 'undefined' || typeof num === 'NaN' || typeof num === 'Infinity' || Math.sign(num) === -1 ) return new Error ('No number'); // проверка аргумента на число
     let newArr = [];
     for (let i = 1; i <= num; i++) {
         newArr.push(i);
