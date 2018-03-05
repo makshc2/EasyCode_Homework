36b6bad57ad1fc592f1cc4c2126379037b88efe2
makshc2 <makshc2@gmail.com>
2018-03-05 20:00:30 +0200

    fix bugs

--- Домашняя работа №9/js/scripts.js
+++ Домашняя работа №9/js/scripts.js
@@ -40,7 +40,6 @@ class VideoPlayer {
 
     clickedBar(e){
         this.video.currentTime = (e.offsetX / this.progress.offsetWidth) * this.video.duration;
-        console.log(this.video.currentTime);
     }
 
     skip(e){
