d5724b11b64c1972372fe8037985f34eec358775
makshc2 <makshc2@gmail.com>
2018-03-05 19:11:35 +0200

    Home work #9

--- Домашняя работа №9/js/scripts.js
+++ Домашняя работа №9/js/scripts.js
@@ -7,6 +7,7 @@ class VideoPlayer {
         this.toggle = this.player.querySelector('.toggle');
         this.skipButtons = this.player.querySelectorAll('[data-skip]');
         this.ranges = this.player.querySelectorAll('.player__slider');
+        this.video.addEventListener()
     }
 
     init(){
@@ -16,6 +17,7 @@ class VideoPlayer {
     events(){
         this.video.addEventListener('click', e => this.togglePlay());
         this.toggle.addEventListener('click', e => this.togglePlay());
+        this.progress.addEventListener('click', e => this.clickedBar(e), false);
         this.ranges.forEach(range => range.addEventListener('change', e => this.handleRangeUpdate(e)));
         this.ranges.forEach(range => range.addEventListener('mousemove', e => this.handleRangeUpdate(e)));
         this.skipButtons.forEach(btn => btn.addEventListener('click', e => this.skip(e)));
@@ -23,14 +25,21 @@ class VideoPlayer {
 
     togglePlay(){
         const method = this.video.paused ? 'play' : 'pause';
-        // this.video.play();
-        // this.video.pause();
         this.toggle.textContent = this.video.paused ? '❚ ❚' : '►';
         this.video[method]();
     }
 
     handleRangeUpdate(e){
         this.video[e.target.name] = e.target.value;
+    }
+
+    clickedBar(e){
+        // let mouseX = e.offsetX - this.progress.offsetLeft;
+        // let newTime = mouseX * Math.floor(this.video.duration) / 320;
+        // console.log('newTime',newTime);
+        // this.video.currentTime = newTime;
+        // console.log(this.video.currentTime);
+        // this.progressBar.style.flexBasis = mouseX + 'px';
     }
 
     skip(e){
