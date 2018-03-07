ead1d0abfc57e6e05ee33387201242698db9192f
Maksim Shevjakov <makshc2@gmail.com>
2018-03-07 11:31:03 +0200

    Update scripts.js

--- Домашняя работа №9/js/scripts.js
+++ Домашняя работа №9/js/scripts.js
@@ -1,4 +1,5 @@
 class VideoPlayer {
+    
     constructor(){
         this.player = document.querySelector('.player');
         this.video = this.player.querySelector('.viewer');
@@ -7,17 +8,19 @@ class VideoPlayer {
         this.toggle = this.player.querySelector('.toggle');
         this.skipButtons = this.player.querySelectorAll('[data-skip]');
         this.ranges = this.player.querySelectorAll('.player__slider');
+        this.properties = JSON.parse(localStorage.getItem('myVideo')) || {currentTime: 0, playbackRate: 1, volume: 1};
     }
 
     init(){
         this.events();
+        this.getSettings();
     }
 
     events(){
         this.video.addEventListener('click', e => this.togglePlay(e));
-        this.video.addEventListener('timeupdate', e => this.updateBar(e));
+        this.video.addEventListener('timeupdate', e => {this.updateBar(e); this.saveSettings()});
         this.toggle.addEventListener('click', e => this.togglePlay(e));
-        this.ranges.forEach(range => range.addEventListener('change', e => this.handleRangeUpdate(e)));
+        this.ranges.forEach(range => range.addEventListener('change', e => {this.handleRangeUpdate(e); this.saveSettings()}));
         this.ranges.forEach(range => range.addEventListener('mousemove', e => this.handleRangeUpdate(e)));
         this.skipButtons.forEach(btn => btn.addEventListener('click', e => this.skip(e)));
         this.progress.addEventListener('click', e => this.clickedBar(e));
@@ -45,6 +48,23 @@ class VideoPlayer {
     skip(e){
         this.video.currentTime += parseFloat(e.target.dataset.skip);
     }
+
+    saveSettings() {
+        this.properties.currentTime = this.video.currentTime;
+        this.properties.playbackRate = this.video.playbackRate;
+        this.properties.volume = this.video.volume;
+
+        let serialProp = JSON.stringify(this.properties);
+        localStorage.setItem('myVideo', serialProp);
+    }
+
+    getSettings() {
+        this.video.currentTime = this.properties.currentTime;
+        this.video.playbackRate = this.properties.playbackRate;
+        this.video.volume = this.properties.volume;
+        this.ranges.forEach(range => range.value = this.properties[range.name]);
+    }
+    
 }
 
 const video = new VideoPlayer();
