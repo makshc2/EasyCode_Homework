51dfcbc6142fae872c53d10b125607023b386446
makshc2 <makshc2@gmail.com>
2018-03-15 18:52:47 +0200

    Home work #11 promise

--- Домашняя работа №11/myscript.js
+++ Домашняя работа №11/myscript.js
@@ -5,3 +5,44 @@ function promiseCreator(timeOut, value) {
 }
 const prom = promiseCreator(500, 'OK!');
 prom.then(console.log);
+
+//task #2
+
+const ul = document.getElementById('posts');
+
+fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'})
+    .then(response => {
+        return response.json()
+    })
+    .then(json => {
+        json.forEach(item => {
+            ul.insertAdjacentHTML('beforebegin', `<li class="list-group-item">${item.id} ${item.title}</li>`);
+
+        });
+    })
+    .catch(err => console.log(err));
+
+//task #3
+
+const totalData = {
+    'posts': {},
+    'users': {}
+};
+
+let postsAll = fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'})
+    .then(res => res.json());
+let usersAll = fetch('https://jsonplaceholder.typicode.com/users',{method: 'GET'})
+    .then(res => res.json());
+
+Promise.all([postsAll, usersAll])
+    .then(data => {
+        dataTotal['posts'] = data[0];
+        dataTotal['users'] = data[1];
+
+        return dataTotal
+    })
+    .then(data => {
+        console.log(`total posts: ${data.posts.length}`);
+        console.log(`total users: ${data.users.length}`);
+    })
+    .catch(err => console.log(err));
