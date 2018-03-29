861c2cdca32e8f2ae2e37549a62d0936548991c3
makshc2 <makshc2@gmail.com>
2018-03-29 11:01:06 +0300

    Home work #13.6

---
+++ Домашняя Работа №14/jquery.easyModal.js
@@ -0,0 +1,83 @@
+(function ($){
+    class Modal{
+        constructor(element, options){
+            this.default = {
+                closeClass: 'close-modal',
+                autoClose: false,
+                autoCloseTime: 1000,
+                opacity: 0.7,
+                position: 'center',
+                duration: 500
+            }
+            this.modal = element;
+            this.options = $.extend(this.default, options);
+            this.overlay = $('<div class="overlay"></div>')
+        }
+
+        init(){
+            this.showOverlay();
+            this.showModal();
+            this.events();
+        }
+
+        events(){
+            this.overlay.on('click', (e) => this.closeModal());
+            $(`.${this.options.closeClass}`).on('click', (e) => this.closeModal());
+        }
+
+        showOverlay(){
+            this.overlay.css({
+                'display':'block',
+                'position':'fixed',
+                'top':'0',
+                'left':'0',
+                'right':'0',
+                'bottom':'0',
+                'opacity':'0',
+                'background-color':`rgba(0,0,0,${this.options.opacity})`,
+                'z-index':'999'
+            });
+
+            this.modal.before(this.overlay);
+        }
+
+        showModal(){
+            const halfWidth = this.modal.outerWidth() / 2;
+            const halfHeight = this.modal.outerHeight() / 2;
+
+            this.overlay.animate({
+                opacity: 1
+            }, this.options.duration);
+            this.modal.css({
+                'display':'block',
+                'position':'fixed',
+                'top':'50%',
+                'left':'50%',
+                'z-index':'1000',
+                'opacity':'0',
+                'margin-top': `-${halfHeight}px`,
+                'margin-left': `-${halfWidth}px`
+            }).animate({
+                opacity:1
+            }, this.options.duration)
+        }
+
+        closeModal(){
+            this.overlay.animate({
+                opacity:1
+            }, this.options.duration, () => {
+                this.overlay.css({'display': 'none'});
+            })
+
+            this.modal.animate({
+                opacity:1
+            }, this.options.duration, () => {
+                this.modal.css({'display': 'none'});
+            });
+        }
+    }
+
+    $.fn.easyModal = function (options) {
+        new Modal(this, options).init();
+    }
+}(jQuery));
