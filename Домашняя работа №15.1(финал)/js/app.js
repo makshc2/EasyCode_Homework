186716f913e22e0781e09fe1fb79a258fe49fb73
makshc2 <makshc2@gmail.com>
2018-04-05 11:59:04 +0300

    fix bugs addTasks

--- Домашняя работа №15.1(финал)/js/app.js
+++ Домашняя работа №15.1(финал)/js/app.js
@@ -14,8 +14,9 @@ const notification = Notification;
 const addTaskObserver = new EventObserver();
 const removeTaskObserver = new EventObserver();
 const removeAllTasksObserver = new EventObserver();
+const editTaskObserver = new EventObserver();
 
-// Subscribe on add task event
+// Subscribe on task events
 addTaskObserver.subscribe(localstorage.update);
 addTaskObserver.subscribe(notification.show);
 addTaskObserver.subscribe(ui.checkList);
@@ -28,11 +29,18 @@ removeAllTasksObserver.subscribe(localstorage.update);
 removeAllTasksObserver.subscribe(notification.show);
 removeAllTasksObserver.subscribe(ui.checkList);
 
+editTaskObserver.subscribe(localstorage.update);
+editTaskObserver.subscribe(notification.show);
+editTaskObserver.subscribe(ui.checkList);
+
 // Init elements
 const form = document.forms['addTodoItem'];
 const inputText = form.elements['todoText'];
 const ul = document.querySelector('.list-group');
 const clearBtn = document.querySelector('.clear-btn');
+const searchForm = document.forms['searchTodoItem'];
+const searchInputText = searchForm.elements['searchText'];
+const clearSearch = document.querySelector('.btn-warning');
 
 window.addEventListener('load', function (e) {
     let ls = localstorage.getTasks();
@@ -50,28 +58,42 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     if (!inputText.value) {
-        inputText.classList.add('is-invalid');
+        // show error, is-invalid
     } else {
-        inputText.classList.remove('is-invalid');
         tasks.addTask({ text: inputText.value })
             .then(task => ui.addTask(task))
             .then(() => addTaskObserver.fire({
                 text: 'Новая задача добавлена успешно!',
                 class: 'alert alert-success'
             }));
-        form.reset();
     }
+    form.reset()
 });
 
 ul.addEventListener('click', function (e) {
+    let id = e.target.closest('li').getAttribute('data-id');
+
     if (e.target.classList.contains('delete-item')) {
-        let id = e.target.closest('li').getAttribute('data-id');
         tasks.removeTask(id)
             .then(() => ui.deleteTask(id))
             .then(() => removeTaskObserver.fire({
                 text: 'Задача удалена успешно!',
                 class: 'alert alert-warning'
             }))
+    }
+
+    if (e.target.classList.contains('edit-item')) {
+        if (!e.target.classList.contains('fa-save')) {
+            ui.getFocus(e.target)
+        } else {
+            ui.editTask(e.target)
+                .then(data => tasks.editTask(data))
+                .then(() => editTaskObserver.fire({
+                    text: 'Задача обновлена успешно!',
+                    class: 'alert alert-success'
+                }))
+        }
+
     }
 });
 
@@ -82,4 +104,35 @@ clearBtn.addEventListener('click', function (e) {
             text: 'Все задачи удалены успешно!',
             class: 'alert alert-warning'
         }))
+});
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    tasks.searchItems(searchInputText.value)
+        .then((data) => {
+            ui.deleteAll();
+            return data
+        })
+        .then(data => {
+            data.forEach(task => {
+                ui.addTask(task)
+            })
+        })
+        .then(() => {
+            ui.checkList()
+        });
+});
+
+clearBtn.addEventListener('click', function (e) {
+    tasks.removeAll()
+        .then(() => ui.deleteAll())
+        .then(() => removeAllTasksObserver.fire({
+            text: 'Все задачи удалены успешно!',
+            class: 'alert alert-warning'
+        }))
+});
+
+clearSearch.addEventListener('click', function (e) {
+
+
 });
--- Домашняя работа №15.1(финал)/js/tasks.js
+++ Домашняя работа №15.1(финал)/js/tasks.js
@@ -2,19 +2,19 @@
 const id = Id;
 
 const Tasks = (function () {
-    
+
     let tasks = [];
     let instance;
-    
+
     const getTasks = function () {
         return tasks;
     };
-    
-    const setTasks = function (array) {
+
+    const setTasks = async function (array) {
         tasks = array;
         return tasks;
     };
-    
+
     const addTask = async function (task) {
         task.id = id.generate();
         await tasks.push(task);
@@ -30,13 +30,32 @@ const Tasks = (function () {
         tasks = [];
     };
 
+    const editTask = async function (data) {
+        for( let i = 0; i < tasks.length; i++){
+            if (data.id === tasks[i].id){
+                await tasks.splice(i, 1, data);
+                break
+            }
+        }
+
+        return tasks
+    };
+
+    const searchItems = async function (text) {
+        return tasks.filter(task => {
+            return (task.text.toLowerCase().indexOf(text.toLowerCase()) >= 0);
+        })
+    };
+
     const createInstance = function () {
         return {
             getTasks,
             setTasks,
             addTask,
             removeTask,
-            removeAll
+            removeAll,
+            editTask,
+            searchItems,
         }
     };
 
@@ -45,5 +64,5 @@ const Tasks = (function () {
             return instance || (instance = createInstance());
         }
     }
-    
+
 }());
